ec50c9416e83042efa8cf4203645d7e682c60c2b
marbypangilinan <marbysneaker@gmail.com>
2023-08-22 13:49:47 -0700

    adding input dropdown

--- bart-app/src/App.js
+++ bart-app/src/App.js
@@ -3,13 +3,35 @@ import React, { useState } from 'react';
 import './App.css';
 import fetcthStationData from './bart';
 import bartstations from './bartstations';
+import {Box, colors, FormControl, InputLabel, MenuItem, Select, createTheme, ThemeProvider} from '@mui/material';
+
 
 function App() {
   const [stations, setStations] = useState({bartstations})
+  const [currentStation, setCurrentStation] = useState('12th');
+
+  const handleChange = (event) => {
+    setCurrentStation(event.target.value);
+  };
 
 
-
-
+  const theme = createTheme({
+    palette: {
+      background: {
+        paper: '#fff',
+      },
+      text: {
+        primary: '#173A5E',
+        secondary: '#46505A',
+      },
+      action: {
+        active: '#001E3C',
+      },
+      success: {
+        dark: '#009688',
+      },
+    },
+  });
 
   console.log(stations);
 
@@ -17,6 +39,27 @@ function App() {
     <div className="Main-container">
       <h1>Bart's React App</h1>
       <h3>Stations</h3>
+      <ThemeProvider theme={theme}>
+      <Box sx={{ minWidth: 120}}>
+      <FormControl fullWidth>
+        <InputLabel  id="demo-simple-select-label">Station</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={currentStation}
+          label="Age"
+          onChange={handleChange}
+        >
+          {bartstations.map((station) => {
+            return <MenuItem value={station.name}>{station.full_name}</MenuItem>
+
+          })}
+
+          
+        </Select>
+      </FormControl>
+    </Box>
+    </ThemeProvider>
       
 
     </div>
--- bart-app/src/bart.js
+++ bart-app/src/bart.js
@@ -4,11 +4,19 @@ const API_KEY = 'MW9S-E7SL-26DU-VV8V'
 
 const fetcthStationData = async (station) => {
     const response = await fetch(' https://api.bart.gov/api/etd.aspx?');
-    const params = {
+    const params = new URLSearchParams({
         cmd: 'etd',
         orig: station,
         key: 'MW9S-E7SL-26DU-VV8V',
         json: 'y'
+    })
+    try {
+        const response = await fetch(' https://api.bart.gov/api/etd.aspx?' + params);
+        const data = await response.json();
+        return data;
     }
-    return data.root.stations.station;
+    catch (error) {
+        console.log(error);
     }
+    
+}
