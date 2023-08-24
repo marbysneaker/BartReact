839b3e4f39dcbae9fafcc6ccab5fb9509a2830a7
Marcelino Pangilinan <marbysneaker@gmail.com>
2023-08-23 23:50:09 -0700

    undoing errors

--- bart-app/src/App.js
+++ bart-app/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import fetcthStationData from './bart';
 import bartstations from './bartstations';
 import {Box, colors, FormControl, InputLabel, MenuItem, Select, createTheme, ThemeProvider} from '@mui/material';
+import { blue } from '@mui/material/colors';
 
 
 function App() {
@@ -14,24 +15,8 @@ function App() {
     setCurrentStation(event.target.value);
   };
 
+  
 
-  const theme = createTheme({
-    palette: {
-      background: {
-        paper: '#fff',
-      },
-      text: {
-        primary: '#173A5E',
-        secondary: '#46505A',
-      },
-      action: {
-        active: '#001E3C',
-      },
-      success: {
-        dark: '#009688',
-      },
-    },
-  });
 
   console.log(stations);
 
@@ -39,27 +24,8 @@ function App() {
     <div className="Main-container">
       <h1>Bart's React App</h1>
       <h3>Stations</h3>
-      <ThemeProvider theme={theme}>
-      <Box sx={{ minWidth: 120}}>
-      <FormControl fullWidth>
-        <InputLabel  id="demo-simple-select-label">Station</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={currentStation}
-          label="Age"
-          onChange={handleChange}
-        >
-          {bartstations.map((station) => {
-            return <MenuItem value={station.name}>{station.full_name}</MenuItem>
-
-          })}
-
-          
-        </Select>
-      </FormControl>
-    </Box>
-    </ThemeProvider>
+      
+    
       
 
     </div>
