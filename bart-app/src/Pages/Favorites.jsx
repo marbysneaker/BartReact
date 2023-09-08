6cace4bd883534f59571bb82fc3cdb7098c669e7
Marcelino Pangilinan <marbysneaker@gmail.com>
2023-09-08 01:12:47 -0700

    fixing favorites css

--- bart-app/src/Pages/Favorites.jsx
+++ bart-app/src/Pages/Favorites.jsx
@@ -1,57 +1,51 @@
-import React, {useEffect, useState} from 'react'
-import './Favorites.css'
-import { UserAuth } from '../context/AuthContext'
-import {doc, getDoc} from 'firebase/firestore'
-import {db} from '../firebase'
-import bartstations from '../bartstations'
-import fetchStationData from '../bart'
-import { initializeApp } from 'firebase/app';
+import React, { useEffect, useState } from "react";
+import "./Favorites.css";
+import { UserAuth } from "../context/AuthContext";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import bartstations from "../bartstations";
+import fetchStationData from "../bart";
+import { initializeApp } from "firebase/app";
 
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-
-
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const Favorites = () => {
-    const {user} = UserAuth();
-    const [favorites, setFavorites] = useState([]);
-    const [stationData, setStationData] = useState([]);
-    const [loadingUser, setLoadingUser] = useState(true);
+  const { user } = UserAuth();
+  const [favorites, setFavorites] = useState([]);
+  const [stationData, setStationData] = useState([]);
+  const [loadingUser, setLoadingUser] = useState(true);
 
-   
+  const fetchSched = async () => {
+    if (!favorites || favorites.length === 0) return;
 
-    const fetchSched = async () => {
-            if (!favorites || favorites.length === 0) return;
-        
-            try {
-              const allData = await Promise.all(
-                favorites.map(async (station) => {
-                  const data = await fetchStationData(station);
-                  if (data.root && data.root.station[0]) {
-                    return {
-                      name: data.root.station[0].name,
-                      trains: data.root.station[0].etd,
-                    };
-                  }
-                  return null;
-                })
-              );
-        
-              // Remove null values
-              const filteredData = allData.filter(Boolean);
-              setStationData(filteredData);
-            } catch (error) {
-              console.error(error);
-            }
-          };
-
-    
-
-      const getUser = async () => {
-        if(!user || !user.email) {
-          console.log("trigger4")
-        }
     try {
-      const docRef = doc(db, 'users', user.email);
+      const allData = await Promise.all(
+        favorites.map(async (station) => {
+          const data = await fetchStationData(station);
+          if (data.root && data.root.station[0]) {
+            return {
+              name: data.root.station[0].name,
+              trains: data.root.station[0].etd,
+            };
+          }
+          return null;
+        })
+      );
+
+      // Remove null values
+      const filteredData = allData.filter(Boolean);
+      setStationData(filteredData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getUser = async () => {
+    if (!user || !user.email) {
+      console.log("trigger4");
+    }
+    try {
+      const docRef = doc(db, "users", user.email);
       const docSnap = await getDoc(docRef);
       console.log("doc snap", docSnap);
 
@@ -62,111 +56,100 @@ const Favorites = () => {
           setFavorites(favoritesData);
         }
       } else {
-        console.log('No such document!');
+        console.log("No such document!");
       }
     } catch (error) {
-      console.error('An error occurred:', error);
+      console.error("An error occurred:", error);
     }
   };
 
+  useEffect(() => {
+    const auth = getAuth(); // Get the authentication instance
 
+    // Set up the listener for authentication state changes
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setLoadingUser(false); // Set loadingUser to false once user data is determined
+    });
 
-
-    useEffect(() => {
-      const auth = getAuth(); // Get the authentication instance
-  
-      // Set up the listener for authentication state changes
-      const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-          setLoadingUser(false); // Set loadingUser to false once user data is determined
-      });
-  
-      // Cleanup the listener on component unmount
-      return () => unsubscribe();
+    // Cleanup the listener on component unmount
+    return () => unsubscribe();
   }, []);
-    useEffect(()=>{
-        
-        const fetchData = async () => {
-          if(!user || !user.email) {
-            console.log("User not found")
-          }
-          if (!loadingUser && user && user.email) {
-            
-            await getUser();
-            
-            await fetchSched();
-           
-          }
-          
-        }
-
-        fetchData();
-
-    },[loadingUser, user])
-
-    // console.log("all stations", stationData);
-    // console.log(bartstations)
-    console.log("favorites",favorites);
-    console.log("users", user);
-
-
-   
-
-    useEffect(() => {
-      if (favorites && favorites.length > 0) {
-          fetchSched();
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!user || !user.email) {
+        console.log("User not found");
       }
+      if (!loadingUser && user && user.email) {
+        await getUser();
+
+        await fetchSched();
+      }
+    };
+
+    fetchData();
+  }, [loadingUser, user]);
+
+  // console.log("all stations", stationData);
+  // console.log(bartstations)
+  console.log("favorites", favorites);
+  console.log("users", user);
+
+  useEffect(() => {
+    if (favorites && favorites.length > 0) {
+      fetchSched();
+    }
   }, [favorites]);
-  
 
   return (
-    <div className='favorites'>
-        {loadingUser ? (
-            <div>Loading...</div>
-        ) : (
-            <>  <div className='favorites-header'>
-                <h1>Favorites</h1>
-                </div>
-                <div className='favorites-stations-container'>
-                {stationData.map((station) => {
-                    return (
-                        <React.Fragment key={station.name}>
-                            <div className='favorites-station-name'>{station.name}</div>
-                            
-                            <div className="favorites-trains-container">
-                                {/* Check if station.trains is defined and is an array before mapping */}
-                                {Array.isArray(station.trains) && station.trains.map((train) => {
-                                    return (
-                                        <div className="train" key={train.destination}>
-                                          <div>{train.destination}</div>
-                                           
-                                        {train.estimate && train.estimate[0] ? (
-                                          <>
-                                            <div className='time'>{train.estimate[0].minutes}</div>
-                                            <div className='direction'>{train.estimate[0].direction}</div>
-                                          </>
-                                        ) : (
-                                          <div className='nodata'>No data</div>
-                                        )}
-                                        </div>
-                                    );
-                                })}
-                            </div>
-                        </React.Fragment>
-                    );
-                })}
-                </div>
-            </>
-        )}
+    <div className="favorites">
+      {loadingUser ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {" "}
+          <div className="favorites-header">
+            <h1>Favorites</h1>
+          </div>
+          <div className="favorites-stations-container">
+            {stationData.map((station) => {
+              return (
+                <div className="favorites-station" key={station.name}>
+                <React.Fragment key={station.name}>
+                  <div className="favorites-station-name">{station.name}</div>
+
+                  <div className="favorites-trains-container">
+                    {/* Check if station.trains is defined and is an array before mapping */}
+                    {Array.isArray(station.trains) &&
+                      station.trains.map((train) => {
+                        return (
+                          <div className="train" key={train.destination}>
+                            <div>{train.destination}</div>
+
+                            {train.estimate && train.estimate[0] ? (
+                              <>
+                                <div className="time">
+                                  {train.estimate[0].minutes}
+                                </div>
+                                <div className="direction">
+                                  {train.estimate[0].direction}
+                                </div>
+                              </>
+                            ) : (
+                              <div className="nodata">No data</div>
+                            )}
+                          </div>
+                        );
+                      })}
+                  </div>
+                </React.Fragment>
+              </div>
+              );
+            })}
+          </div>
+        </>
+      )}
     </div>
-);
+  );
+};
 
-}
-
-export default Favorites
-
-
-
-
-
-
-
+export default Favorites;
